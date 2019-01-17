774f6ce305513b0ba133dbd50f3c2ec4e2295913
angelabian <angelali8321@gmail.com>
2019-01-17 21:52:05 +0800

    蘇軾主題跟體驗程式分開；上傳圖片後換掉原圖

--- php/uploadMain.php
+++ php/uploadMain.php
@@ -1,11 +1,11 @@
 <?php
 $target_dir = "../img/customer/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$target_file = $target_dir . basename($_FILES["userImage"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    $check = getimagesize($_FILES["userImage"]["tmp_name"]);
     if($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
@@ -14,13 +14,8 @@ if(isset($_POST["submit"])) {
         $uploadOk = 0;
     }
 }
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
+if ($_FILES["userImage"]["size"] > 500000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
@@ -35,8 +30,10 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    if (move_uploaded_file($_FILES["userImage"]["tmp_name"], $target_file)) {
+        ?>
+        <img class='baseIMG' src="<?php echo "img/customer/".$_FILES['userImage']['name']; ?>" />
+        <?php
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
